990c5a0d2d40ee5e81fb483ca705ee849fdb129f
WAVEKUB <itsarawut.happy@gmail.com>
2025-12-11 10:59:52 +0700

    feat: add initial user and authentication API routes

--- backend/routes/userRoutes.go
+++ backend/routes/userRoutes.go
@@ -1,11 +1,17 @@
 package routes
 
 import (
+	"github.com/WAVEKUB/fintrack-backend/controllers"
+	"github.com/WAVEKUB/fintrack-backend/initializers"
 	"github.com/WAVEKUB/fintrack-backend/middleware"
+	"github.com/WAVEKUB/fintrack-backend/services"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(router *gin.Engine) {
+
+	userService := services.NewUserService(initializers.DB)
+	usersController := controllers.NewUsersController(userService)
 
 	userRoutes := router.Group("/user")
 	userRoutes.Use(middleware.RequireAuth)
